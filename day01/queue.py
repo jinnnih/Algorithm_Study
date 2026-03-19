2fd45c3a3ba13bf87fa7b9e71b13c755cf06af5b
hyunjin <jinnnih@gmail.com>
2026-03-19 11:48:57 +0900

    stack, queue 실습

--- day01/queue.py
+++ day01/queue.py
@@ -10,11 +10,11 @@
 from collections import deque
 
 queue = deque() # 앞의 데이터를 꺼냄
-queue.append('A') # enqueue A -> deque
-queue.append('B') # enqueue B -> deque
-queue.append('C') # enqueue C -> deque
+queue.append('A') # enqueue A -> deque (['A']
+queue.append('B') # enqueue B -> deque (['A','B']
+queue.append('C') # enqueue C -> deque (['A','B','C']
 print(queue)
-print(queue.popleft())
+print(queue.popleft()) # -> 'A' (가장 먼저 넣은 A가 먼저나옴)
 print(queue)
-print(queue.popleft())
+print(queue.popleft()) # -> 'B'
 print(queue)
